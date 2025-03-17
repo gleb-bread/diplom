e00a1bac02cfb7224bef70175e1ac8bdb60c51c7
gleb-bread <96437775+gleb-bread@users.noreply.github.com>
2025-03-17 20:50:18 +0300

    fix double components

--- src/pages/page.vue
+++ src/pages/page.vue
@@ -1,13 +1,25 @@
 <script lang="ts" setup>
 import { usePageStore } from '@/app/stores/page';
 import { useComponentStore } from '@/app/stores/components';
-import { computed } from 'vue';
+import { computed, defineComponent } from 'vue';
 import FactoryComponent from '@/widgets/components/FactoryComponent.vue';
+import { Actions } from '@/shared/actions';
+import { Config } from '@/shared/config';
+import { Handlers } from '@/shared/handlers';
+
+const nameComponent = 'WrapperComponent';
+
+defineComponent({ name: nameComponent });
+
+const config = new Config.Actions.Config(nameComponent);
 
 const componentStore = useComponentStore();
 const pageStore = usePageStore();
 
+const pageId = pageStore.getSelectPage;
+
 const getSelectPage = computed(() => pageStore.getSelectPage);
+const handlerCreateComponent = new Handlers.CreateNewComponent(pageId);
 
 const getComponentIds = computed(
     () => componentStore.getGenericList[getSelectPage.value]
@@ -15,7 +27,19 @@ const getComponentIds = computed(
 </script>
 
 <template>
-    <v-list :bg-color="$STYLE_VARIBLES.COLOR.BACKGROUND" class="px-2">
+    <v-list
+        @click.stop="
+            $ACTION_MANAGER.pushAction(
+                new Actions.Click.ClickAction(
+                    $event,
+                    config,
+                    handlerCreateComponent
+                )
+            )
+        "
+        :bg-color="$STYLE_VARIBLES.COLOR.BACKGROUND"
+        class="px-2"
+    >
         <template v-for="id in getComponentIds">
             <factory-component :component-id="id" :page-id="getSelectPage">
             </factory-component>
